4f7f03ecc6f4cceba5c88eaf4e292faeca407398
MrAlladeen <felixschmidt95@gmail.com>
2017-12-05 14:58:50 +0100

    server code 2017/11/30

--- server.js
+++ server.js
@@ -5,18 +5,29 @@ var express = require('express'),
     io = io.listen(server),
     uniqueid = require('uniqid'),
     async = require('async'),
+    fs = require('fs'),
     port = process.env.PORT || 3000,
     logged_users = {},
     logged_profs = {},
     get_users_by_ID = {},
-    IDs = [];
+    IDs = [],
+    file,
+    ad_image,
+    logger = fs.createWriteStream('ads_log.txt',{
+        flags: 'a'  //a means appending
+    });
 
 
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
+
+    //file = fs.readFileSync('./HIERDERNAMEDERDATEI.jpg');//todo enetr right path and uncomment both lines!
+    //ad_image = new Buffer(file).toString('base64');
+
 });
 
 app.use(express.static(__dirname));
+
 
 io.on('connection', function (socket) {
     console.log('connected');
@@ -28,8 +39,8 @@ io.on('connection', function (socket) {
         uniqueID = uniqueID.substring(4,8);
 
         logged_profs[uniqueID] = socket;
-
         IDs.push(uniqueID);
+
         socket.emit('IDcreated',uniqueID);
     });
 
@@ -48,39 +59,31 @@ io.on('connection', function (socket) {
     });
 
     socket.on('singlePic', function(user, bitmap){
-        logged_users[user].emit('singlePic', bitmap);
+        logged_users[user].emit('singlePic', bitmap);   //send client his requested image
     });
 
     //user
     socket.on('userLoginWithSessionID', function(ID){
-        console.log('userLoginWithSessionID: ' + ID);
-
         var uniqueID = uniqueid.time();
         uniqueID = uniqueID.substring(4,8);
 
+        //check if such session exists
         if (IDs.indexOf(ID) > -1) {
-            //In the array!
             //saving session ID and socket
             get_users_by_ID[uniqueID] = ID;
             logged_users[uniqueID] = socket;
 
-            socket.emit('userID', uniqueID);
-            console.log('in IDs');
-        } else {
-            //Not in the array!
+            socket.emit('userID', uniqueID);    //send off username, login
+            socket.emit('adImage', ad_image);   //send off add
+
+            logger.write('1');                  //simply an ad access counter that adds '1' every time a user 'gets' ad
+        }
+        else {
             socket.emit('errorTypingSessionID');
-            console.log('not in IDs');
         }
     });
 
     socket.on('singlePicRequest', function(user, sessionID){
-        logged_profs[sessionID].emit('singlePicRequest', user);
+        logged_profs[sessionID].emit('singlePicRequest', user);     //inform presenter client to share an 'instant image' of his content
     });
-
-    //todo save ips automatically
-    socket.on('saveMyIPAndPagesICreated',function(json){
-       //save data for proving usage of app to advertisers
-    });
-
-    //todo ads
 });
